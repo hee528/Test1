8ad1de1a168542e0f365ffc8552e721af33ac7ed
Hee <hee528@gmail.com>
2018-04-23 22:44:27 +0900

    두둥

--- blockchain.rb
+++ blockchain.rb
@@ -1,3 +1,9 @@
-class Blockchain #전체 생태계 
-	attr_reader :chain, :transactions 
+class Blockchain #전체 생태계를 선언함 / 대문자로 시작해야 
+
+	attr_reader :chain, :transactions #attribute reader / 콤마로 구분 
+
 end
+
+variable = Blockchain.new 
+
+puts variable
