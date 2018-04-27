e9ae6a08b9612199b9697c82dd204904075d67b0
Hee <hee528@gmail.com>
2018-04-27 22:40:41 +0900

    4/27 역사적인 날 코딩 수업 결과물!

--- server.rb
+++ server.rb
@@ -8,13 +8,14 @@ get '/' do			  # get / do prints out to a local host
 
 	message = ""
 
-	b.all_chains.each do |a| #모든 블록들이 매번 A라는 이름으로 저장돼서 돌게 됨
+	b.all_chains.each do |a| # 모든 블록들이 매번 A라는 이름으로 저장돼서 돌게 됨
 
-	message << "번호는 : " + a['index'].to_s + "<br>"  
+	message << "번호는 : " + a['index'].to_s + "<br>"  	# a is the given block
 	message << "Nonce는 : " + a['nonce'].to_s + "<br>"
 	message << "시간은 : " + a['time'].to_s + "<br>"	
 	message << "앞 주소는 : " + a['previous_address'].to_s + "<br>"
-	message << "<hr>"
+	message << "내 주소는 : " + Digest::SHA256.hexdigest(a.to_s) + "<br>"	# 블럭 해쉬 값  
+	message << "<hr>"	# horizon rule
 
 	end 
 
@@ -23,6 +24,6 @@ message
 end
 
 get '/mine' do
-	b.mining.to_s     #this brings the last value of the "mining" part of the block file
+	b.mining.to_s     # this brings the last value of the "mining" part of the block file
 					  # to.s changes the number '0' to text '0' (string)
 end
