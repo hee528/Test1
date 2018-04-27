40fdca14482a8e7c8d92221f285097f670550645
Hee <hee528@gmail.com>
2018-04-27 21:35:50 +0900

    이두히 바보

--- block.rb
+++ block.rb
@@ -1,14 +1,25 @@
 
-class Blockchain
+class Blockchain               #172608			   
 
 	def mining
 
-    	begin 
+		history = []		   #history has a list. its left empty.
 
-    		nonce = rand(10)
 
-    	end while nonce != 0 
 
+    	begin 					# In ruby, BEGIN/END is the same as "Loop"
+
+    		nonce = rand(100000) # 일반 블럭체인은 이렇게 상한선을 정하지 않음 
+
+    		history << nonce	#nonce값이 나올때마다 history에 밀어 넣기 
+
+    		hashed = Digest::SHA256.hexdigest(nonce.to_s) # HASHED value equals nonce (in strong), encrypted using SHA256
+
+    	end while hashed[0..3] != "0000" # take the first four digits (0~3) of the hashed value. loop until the value is "0000"
+    								# 채굴 난이도는 이 라인에서 결정됨. 하지만 함부로 쓰면 무한 루프 됨ㅠ 
+
+    	 history.size 		# this prints out to screen. PUTS prints to black screen
+    						# this method prints to a web screen
 	end
 
 end
--- server.rb
+++ server.rb
@@ -1,13 +1,14 @@
 
-require 'sinatra'
-require './block'
+require 'sinatra'     # this declares that we are using the web frame SINATRA
+require './block'     # this calls the block file
 
-b = Blockchain.new
+b = Blockchain.new    # we declared a class called 'b'
 
-get '/' do
+get '/' do			  # get / do prints out to a local host
 	"블럭 리스트 입니다."
 end
 
 get '/mine' do
-	"마이닝 중입니다."
+	b.mining.to_s     #this brings the last value of the "mining" part of the block file
+					  # to.s changes the number '0' to text '0' (string)
 end
